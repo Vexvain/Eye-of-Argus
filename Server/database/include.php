d326122cdc6f9476b17419cdcb0da42e5697e56a
Vexvain <60254325+Vexvain@users.noreply.github.com>
2020-11-02 03:02:20 -0500

    Update include.php

--- Server/database/include.php
+++ Server/database/include.php
@@ -1,11 +1,8 @@
 <?php	
 	/*
-		CAESAR
+		ARGUS
 
-		Author : Michele '0blio' Cisternino
-		Email  : miki.cisternino@gmail.com
-		Github : https://github.com/0blio
-	
+		Author : VEXVAIN
 		This project is released under the GPL 3 license. 	
 	*/
 
@@ -13,18 +10,18 @@
 	include 'PicoDb/SQLException.php';
 
 	// Builders 
-	include 'PicoDb/Builder/BaseBuilder.php';
-	include 'PicoDb/Builder/InsertBuilder.php';
-	include 'PicoDb/Builder/UpdateBuilder.php';
-	include 'PicoDb/Builder/ConditionBuilder.php';
-	include 'PicoDb/Builder/OrConditionBuilder.php';
+	include 'Db/Builder/BaseBuilder.php';
+	include 'Db/Builder/InsertBuilder.php';
+	include 'Db/Builder/UpdateBuilder.php';
+	include 'Db/Builder/ConditionBuilder.php';
+	include 'Db/Builder/OrConditionBuilder.php';
 
 	// Drivers
-	include 'PicoDb/Driver/Base.php';
-	include 'PicoDb/DriverFactory.php';
-	include 'PicoDb/Driver/Mysql.php';
+	include 'Db/Driver/Base.php';
+	include 'Db/DriverFactory.php';
+	include 'Db/Driver/Mysql.php';
 
 	// Database
-	include 'PicoDb/StatementHandler.php';
-	include 'PicoDb/Database.php';
-	include 'PicoDb/Table.php';
+	include 'Db/StatementHandler.php';
+	include 'Db/Database.php';
+	include 'Db/Table.php';
